9d9f454554d0a353be8624c399f8ef248ed9dc8a
Federico Longhin <65185337+FedericoLonghin@users.noreply.github.com>
2022-04-07 20:36:54 +0200

    fully working (responsive) card system (frontend)

--- src/CardLoader.js
+++ src/CardLoader.js
@@ -1,6 +1,6 @@
-    document.getElementById("card-1-title").innerHTML = "Andamento CO1";
-    document.getElementById("card-2-title").innerHTML = "Andamento CO2";
-    document.getElementById("card-3-title").innerHTML = "Andamento CO3";
-    document.getElementById("card-4-title").innerHTML = "Andamento CO4";
-    document.getElementById("card-5-title").innerHTML = "Andamento CO5";
-    document.getElementById("card-6-title").innerHTML = "Andamento CO6";
+  document.getElementById("card-1-title").innerHTML = "Percentuale CO2";
+  document.getElementById("card-2-title").innerHTML = "Temperatura ingresso Aria";
+
+
+  document.getElementById("card-1-body").innerHTML = "35%";
+  document.getElementById("card-2-body").innerHTML = "20°C";
